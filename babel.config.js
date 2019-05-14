5fb95fbdf35235306d59044c8e4ce81098abeb85
zhuanghaixin <649308293@qq.com>
2019-05-15 04:02:18 +0800

    [antd pro]:配置webpack,babel,添加button组件

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: ["@vue/app"]
+  presets: ["@vue/app"],
+  plugins: [["import", { libraryName: "antd", style: true }]]
 };
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
+// eslint-disable-next-line
+/* eslint-disable */
 import Vue from "vue";
 import App from "./App.vue";
+import { Button } from "ant-design-vue";
 import router from "./router";
 import store from "./store";
+import "ant-design-vue/dist/antd.less";
 
 Vue.config.productionTip = false;
-
+Vue.use(Button);
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
